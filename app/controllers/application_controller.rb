c9987f2bb96cf0560c03a232e7434ec625a3ed7d
EC2 Default User <ec2-user@ip-172-31-10-86.ec2.internal>
2022-03-15 07:00:02 +0000

    log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # ストロングパラメータと同様の機能
+# devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
+# configure_permitted_parametersメソッドが実行されます。
+   before_action :configure_permitted_parameters, if: :devise_controller?
+# protectedは呼び出された他のコントローラからも参照することができます。
+  protected
+# configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことで
+# ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
