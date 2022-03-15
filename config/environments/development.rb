b47f9569af35dd90ec4b530a45e3b005c5d1dbca
EC2 Default User <ec2-user@ip-172-31-10-86.ec2.internal>
2022-03-15 06:19:37 +0000

    devise install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 config.hosts << "c35af7e51da740048a27d1f7ac61b683.vfs.cloud9.us-east-1.amazonaws.com"
+
+ config.active_job.queue_adapter = :inline
 end
